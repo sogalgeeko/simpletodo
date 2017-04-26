sogalgeeko/simpletodo

commit d6c4346d51bd2e12258f51a1c224e6a0a6597ad0

fix delete row on task move, updated readme

simpletodo.py
@@ -353,7 +353,7 @@ class HeaderBarWindow(Gtk.Window):
         to target then removing it"""
         task_content = self.get_current_child().get_selected_task()
         if task_content:
-            self.get_current_child().on_row_delete()
+            self.get_current_child().on_row_delete(self.update_percent_on_check)
             for child in self.tnotebook:
                 project_name = self.tnotebook.get_tab_label_text(child)
                 page_index = self.tnotebook.page_num(child)

commit 284b8ea099b37b84173ae5722ec9d33269699585

fix task creation and project renaming broken by d6c4346d51bd2e12258f51a1c224e6a0a6597ad0

simpletodo.py
@@ -259,7 +259,8 @@ class HeaderBarWindow(Gtk.Window):
         project tab label text"""
         self.new_name_input = InputWin("Renommer le projet « " +
                                        self.get_project_name() + " »",
-                                       self.on_rename_project)
+                                       self.on_rename_project,
+                                       self.update_percent_on_check)
 
     def on_rename_project(self, text, *args):
         """Change tab label (rename project)"""
@@ -365,7 +366,7 @@ class HeaderBarWindow(Gtk.Window):
         """Define action to perform on keyboard shortcut,
         here Ctrl + n launches task creation in active page list"""
         self.get_current_child().on_launch_creation(
-            self.get_project_name())
+            self.get_project_name(), self.update_percent_on_check)
 
     def accel_edit_mode_on(self, *args):
         """On keyboard shortcut, toggle switch state,